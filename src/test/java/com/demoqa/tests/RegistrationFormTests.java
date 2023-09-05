9c15f76be16df9bf06941d06d8f9c078253123cd
Yulia Azovtseva <yulia.pryania@gmail.com>
2023-09-05 13:14:06 +0400

    remote launching added

--- src/test/java/com/demoqa/tests/RegistrationFormTests.java
+++ src/test/java/com/demoqa/tests/RegistrationFormTests.java
@@ -2,11 +2,20 @@ package com.demoqa.tests;
 
 import com.demoqa.pages.RegistrationFormPage;
 import com.demoqa.pages.components.ProfilePreviewModal;
+import com.demoqa.utils.RegistrationFormDataGenerator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class RegistrationFormTests extends TestBase {
+import java.util.Date;
+import java.util.List;
+
+import static io.qameta.allure.Allure.step;
+
+@DisplayName("Registration form tests")
+public class RegistrationFormTests extends TestBaseRemote {
     @Test
-    void fillAllFieldsTest() {
+    @DisplayName("Successful registration with all fields validly filled")
+    void successfulRegistrationWithValidFields() {
         RegistrationFormPage registrationFormPage = new RegistrationFormPage();
         ProfilePreviewModal profilePreviewModal = new ProfilePreviewModal();
 
@@ -27,36 +36,94 @@ public class RegistrationFormTests extends TestBase {
                 stateName = "Uttar Pradesh",
                 cityName = "Agra";
 
-        registrationFormPage.openPage()
-                .removeBanners()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(userEmail)
-                .setGender(gender)
-                .setUserNumber(userPhoneNumber)
-                .setBirthDate(birthdayYear, birthdayMonth, birthdayDay)
-                .setSubject(subject1)
-                .setSubject(subject2)
-                .setHobby(hobby1)
-                .setHobby(hobby2)
-                .uploadPicture(pictureFileFromClassPath)
-                .setCurrentAddress(currentAddress)
-                .setState(stateName)
-                .setCity(cityName)
-                .clickSubmitButton();
+        step("Open form", () ->
+                registrationFormPage.openPage()
+                        .removeBanners()
+        );
+        step("Fill all fields", () ->
+                registrationFormPage.setFirstName(firstName)
+                        .setLastName(lastName)
+                        .setUserEmail(userEmail)
+                        .setGender(gender)
+                        .setUserNumber(userPhoneNumber)
+                        .setBirthDate(birthdayYear, birthdayMonth, birthdayDay)
+                        .setSubject(subject1)
+                        .setSubject(subject2)
+                        .setHobby(hobby1)
+                        .setHobby(hobby2)
+                        .uploadPicture(pictureFileFromClassPath)
+                        .setCurrentAddress(currentAddress)
+                        .setState(stateName)
+                        .setCity(cityName)
+                        .clickSubmitButton()
+        );
+        step("Verify results", () ->
+                profilePreviewModal.textIsVisible(firstName + " " + lastName)
+                        .textIsVisible(userEmail)
+                        .textIsVisible(gender)
+                        .textIsVisible(userPhoneNumber)
+                        .textIsVisible(birthdayDay + " " + birthdayMonth + "," + birthdayYear)
+                        .textIsVisible(subject1)
+                        .textIsVisible(subject2)
+                        .textIsVisible(hobby1)
+                        .textIsVisible(hobby2)
+                        .textIsVisible(pictureFileFromClassPath)
+                        .textIsVisible(currentAddress)
+                        .textIsVisible(stateName + " " + cityName)
+        );
+    }
 
-        profilePreviewModal.textIsVisible(firstName + " " + lastName)
-                .textIsVisible(userEmail)
-                .textIsVisible(gender)
-                .textIsVisible(userPhoneNumber)
-                .textIsVisible(birthdayDay + " " + birthdayMonth + "," + birthdayYear)
-                .textIsVisible(subject1)
-                .textIsVisible(subject2)
-                .textIsVisible(hobby1)
-                .textIsVisible(hobby2)
-                .textIsVisible(pictureFileFromClassPath)
-                .textIsVisible(currentAddress)
-                .textIsVisible(stateName + " " + cityName);
+    @Test
+    @DisplayName("Successful registration with all fields validly filled (generated data)")
+    void successfulRegistrationWithGeneratedData() {
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+        ProfilePreviewModal profilePreviewModal = new ProfilePreviewModal();
+        RegistrationFormDataGenerator randomData = new RegistrationFormDataGenerator();
+
+        Date birthDate = randomData.generateBirthDate();
+        List<String> hobbies = randomData.generateHobby(2);
+        List<String> subjects = randomData.generateSubject(3);
+        String firstName = randomData.generateFirstName(),
+                lastName = randomData.generateLastName(),
+                userEmail = randomData.generateUserEmail(),
+                userPhoneNumber = randomData.generatePhoneNumber(),
+                gender = randomData.generateGender(),
+                currentAddress = randomData.generateAddress(),
+                pictureFileFromClassPath = randomData.generatePicture(),
+                stateName = randomData.generateState(),
+                cityName = randomData.generateCity(stateName);
+
+        step("Open form", () ->
+                registrationFormPage.openPage()
+                        .removeBanners()
+        );
+        step("Fill all fields", () ->
+                registrationFormPage.setFirstName(firstName)
+                        .setLastName(lastName)
+                        .setUserEmail(userEmail)
+                        .setGender(gender)
+                        .setUserNumber(userPhoneNumber)
+                        .setBirthDate(birthDate)
+                        .setSubject(subjects)
+                        .setHobby(hobbies)
+                        .uploadPicture(pictureFileFromClassPath)
+                        .setCurrentAddress(currentAddress)
+                        .setState(stateName)
+                        .setCity(cityName)
+                        .clickSubmitButton()
+        );
+        step("Verify results", () ->
+                profilePreviewModal.textIsVisible(firstName + " " + lastName)
+                        .textIsVisible(userEmail)
+                        .textIsVisible(gender)
+                        .textIsVisible(userPhoneNumber)
+                        .dateIsVisible(birthDate)
+                        .listItemsAreVisible(subjects)
+                        .listItemsAreVisible(hobbies)
+                        .textIsVisible(pictureFileFromClassPath)
+                        .textIsVisible(currentAddress)
+                        .textIsVisible(stateName + " " + cityName)
+        );
     }
 }
 
---
+++ src/test/java/com/demoqa/tests/TestBaseRemote.java
@@ -0,0 +1,56 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static org.asynchttpclient.util.HttpConstants.Methods.OPTIONS;
+
+public class TestBaseRemote {
+    @BeforeAll
+    static void init() {
+        baseUrl = "https://demoqa.com";
+        browserSize = "1920x1080";
+        pageLoadStrategy = "eager";
+        remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(OPTIONS);
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    public void addListener() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addArtifactsToReport() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+
+    @AfterEach
+    void closeWebDriver() {
+        closeWebDriver();
+    }
+}
