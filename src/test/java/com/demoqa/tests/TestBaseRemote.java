e9ed5735d3edb49f333f581ca62acaf06d4b339d
Yulia Azovtseva <yulia.pryania@gmail.com>
2023-09-06 13:07:10 +0400

    browser variable changed

--- src/test/java/com/demoqa/tests/TestBaseRemote.java
+++ src/test/java/com/demoqa/tests/TestBaseRemote.java
@@ -11,27 +11,28 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
+import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBaseRemote {
     @BeforeAll
     static void init() {
-        String browser = System.getProperty("browser", "chrome_100.0");
-        String[] browserInfo = browser.split("_");
-        String browserName = browserInfo[0];
-        String browserVersion = browserInfo[1];
+        String browserFromProperties = System.getProperty("browser", "chrome_100.0");
+        String[] browserInfo = browserFromProperties.split("_");
+        String browserFromPropertiesName = browserInfo[0];
+        String browserFromPropertiesVersion = browserInfo[1];
 
-        Configuration.browser = browserName;
-        Configuration.browserVersion = browserVersion;
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-        Configuration.remote = "https://user1:1234@" +
-                                System.getProperty("remoteWebDriver", "selenoid.autotests.cloud") +
-                                "/wd/hub";
+        browser = browserFromPropertiesName;
+        browserVersion = browserFromPropertiesVersion;
+        browserSize = System.getProperty("browserSize", "1920x1080");
+        baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        remote = "https://user1:1234@" +
+                 System.getProperty("remoteWebDriver", "selenoid.autotests.cloud") +
+                 "/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", browserName);
-        capabilities.setCapability("browserVersion", browserVersion);
+        capabilities.setCapability("browserName", browserFromPropertiesName);
+        capabilities.setCapability("browserVersion", browserFromPropertiesVersion);
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
